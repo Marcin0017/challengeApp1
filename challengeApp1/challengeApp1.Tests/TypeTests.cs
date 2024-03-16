9575f3011063c1401f28566be9c0d296d721b732
Marcin0017 <Marcin.Madon1@o2.pl>
2024-03-16 23:00:25 +0100

    zadanie dzień 8

---
+++ challengeApp1/challengeApp1.Tests/TypeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace challengeApp1.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void IntTest()
+        {
+            int number1 = 1;
+            int number2 = 2;
+
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void StringTest()
+        {
+            string name1 = "Marcin";
+            string name2 = "Marcin";
+
+            Assert.AreEqual(name1, name2);
+        }
+
+        [Test]
+        public void RederenceTest()
+        {
+            var employee1 = ("Adam", "XYZ", 33);
+            var employee2 = ("Monika", "XYZ", 22);
+
+            Assert.AreNotEqual(employee1, employee2);
+        }
+        [Test]
+        public void IntAgeTest()
+        {
+            int age1 = 11;
+            int age2 = 19;
+
+            Assert.AreNotEqual(age1, age2);
+        }
+        private Employee GetUser(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    
+    }
+
+}
