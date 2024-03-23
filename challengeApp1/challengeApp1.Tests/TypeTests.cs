cd65ed180567c3c41d9cf5c533ac459ab61a4746
Marcin0017 <Marcin.Madon1@o2.pl>
2024-03-23 17:49:06 +0100

    zadanie dzień 9

--- challengeApp1/challengeApp1.Tests/TypeTests.cs
+++ challengeApp1/challengeApp1.Tests/TypeTests.cs
@@ -3,44 +3,38 @@
     public class TypeTests
     {
         [Test]
-        public void IntTest()
+        public void MethosGetStatisticsMin()
         {
-            int number1 = 1;
-            int number2 = 2;
+            var employee = new Employee1("Adam", "XYZ");
 
-            Assert.AreNotEqual(number1, number2);
+            employee.AddGrade(33);
+            employee.AddGrade(45);
+            employee.AddGrade(21);
+
+            var ststistics = employee.GetStatistics();
+
+            Assert.AreEqual(21, ststistics.Min);
+            Assert.AreEqual(45, ststistics.Max);
+            Assert.AreEqual(33, ststistics.Average);
+
         }
 
         [Test]
-        public void StringTest()
+        public void MethosGetStatisticsMax()
         {
-            string name1 = "Marcin";
-            string name2 = "Marcin";
+            var employee = new Employee1("Adam", "XYZ");
 
-            Assert.AreEqual(name1, name2);
+            employee.AddGrade(12);
+            employee.AddGrade(-10);
+            employee.AddGrade(31);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(-10, statistics.Min);
+            Assert.AreEqual(31, statistics.Max);
+            Assert.AreEqual(11, statistics.Average);
         }
 
-        [Test]
-        public void RederenceTest()
-        {
-            var employee1 = ("Adam", "XYZ", 33);
-            var employee2 = ("Monika", "XYZ", 22);
-
-            Assert.AreNotEqual(employee1, employee2);
-        }
-        [Test]
-        public void IntAgeTest()
-        {
-            int age1 = 11;
-            int age2 = 19;
-
-            Assert.AreNotEqual(age1, age2);
-        }
-        private Employee GetUser(string name, string surname, int age)
-        {
-            return new Employee(name, surname, age);
-        }
-    
     }
 
 }
