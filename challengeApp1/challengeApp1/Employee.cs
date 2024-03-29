ddcc1db82b21d1ee23018f6b77616618180f59d8
Marcin0017 <Marcin.Madon1@o2.pl>
2024-03-29 19:20:54 +0100

    zadanie dzień 10

---
+++ challengeApp1/challengeApp1/Employee.cs
@@ -0,0 +1,83 @@
+﻿namespace challengeApp1
+{
+    public class Employee1
+    {
+        private List<float> grades = new List<float>();
+
+        public Employee1(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+
+        public void AddGrade(float grade)
+        {
+            // 3.99
+            // 3
+            int valueInInt = (int)grade;
+            float f = valueInInt;
+
+            if(grade>= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");   
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            float floatvalue = (float)grade;
+            this.grades.Add(floatvalue);
+        }
+        public void AddGrade(long grade)
+        {
+            float floatvalue = (float)grade;
+            this.grades.Add(floatvalue);
+        }
+        public void AddGrade(int grade)
+        {
+            float floatvalue = (float)grade;
+            this.grades.Add(floatvalue);
+        }
+        public void AddGrade(char grade)
+        {
+            float floatvalue = (float)grade;
+            this.grades.Add(floatvalue); 
+        }
+        public Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;    
+
+            foreach (var grade in this.grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+        }
+    }
+}
