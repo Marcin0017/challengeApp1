cde3b45700bdcd62c99eef630e1a196df2a4288c
Marcin0017 <Marcin.Madon1@o2.pl>
2024-05-23 20:02:16 +0200

    zadanie dzień 15

---
+++ challengeApp1/challengeApp1/IEmployee.cs
@@ -0,0 +1,69 @@
+﻿namespace challengeApp1
+{
+    public interface IEmployee
+    {
+        string Surname { get; }
+        string Name { get; }
+
+        void AddGrade(float grade);
+
+        void AddGrade(int grade);
+
+        void AddGrade(double grade);
+
+        void AddGrade(char grade);
+
+        void AddGrade(string grade)
+        {
+            switch (grade)
+            {
+                case "6":
+                    AddGrade(100);
+                    break;
+                case "6-":
+                case "-6":
+                    AddGrade(95);
+                    break;
+                case "5":
+                    AddGrade(80);
+                    break;
+                case "-5":
+                case "5-":
+                    AddGrade(75);
+                    break;
+                case "4":
+                    AddGrade(60);
+                    break;
+                case "-4":
+                case "4-":
+                    AddGrade(55);
+                    break;
+                case "3":
+                    AddGrade(40);
+                    break;
+                case "3-":
+                case "-3":
+                    AddGrade(35);
+                    break;
+                case "2+":
+                case "+2":
+                    AddGrade(25);
+                    break;
+                case "2":
+                    AddGrade(20);
+                    break;
+                case "-2":
+                case "2-":
+                    AddGrade(15);
+                    break;
+                case "1+":
+                case "+1":
+                    AddGrade(5);
+                    break;
+                default:
+                    throw new Exception("Number should be between 6(max) 1(min)");
+            }
+        }
+        Statistics GetStatistics();
+    }
+}
---
+++ challengeApp1/challengeApp1/Supervisor.cs
@@ -0,0 +1,38 @@
+﻿namespace challengeApp1
+{
+    public class Supervisor : IEmployee
+    {
+        public string Name => "Adam";
+
+        public string Surname => "Kazimielich";
+
+        public void AddGrade(float grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Statistics GetStatistics()
+        {
+            return new Statistics();
+        }
+        public void AddGrade(string grade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
